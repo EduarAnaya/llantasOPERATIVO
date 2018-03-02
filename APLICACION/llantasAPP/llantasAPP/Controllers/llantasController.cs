53b40db7a403f7782e7ed38a61052260ba908566
EduarAnaya <eduar.anaya@hotmail.com>
2018-03-02 18:25:59 -0500

    termino del dia
    Se generan las siguientes vistas
    -vista de opciones
    -vista inventario

---
+++ APLICACION/llantasAPP/llantasAPP/Controllers/llantasController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using llantasAPP.Models;
+
+namespace llantasAPP.Controllers
+{
+    public class llantasController : Controller
+    {
+        //
+        // GET: /llantas/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Inventario()
+        {
+            List<llantasInventario> tablaInvenario = new List<llantasInventario>();
+            llantasInventario rowinventario = new llantasInventario();
+            /*
+             * 
+             * 
+             * 
+             * 
+             */
+
+            tablaInvenario.Add(rowinventario);
+
+            return View(tablaInvenario);
+        }
+    }
+}
---
+++ APLICACION/llantasAPP/llantasAPP/Models/llantasModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace llantasAPP.Models
+{
+
+    public class llantasInventario
+    {
+        public string LLANTA {get;set;}
+        public string GRUPO { get; set; }
+        public int INVENT { get; set; }
+        public int VALOR { get; set; }
+        public DateTime FECHA { get; set; }
+        public int PROVE { get; set; }
+        public int FACTURA { get; set; }
+        public int FICHA { get; set; }
+    }
+}
